a87a0380ba9a2bc54e45cabf48914856ea1a074b
amiralii <mr.roshanae@gmail.com>
2020-02-02 08:03:15 +0330

    fixed

--- repository/arango/message.go
+++ repository/arango/message.go
@@ -6,17 +6,15 @@ import (
 	"github.com/amiraliio/tgbp-api/domain/message"
 )
 
-type messageRepo struct{
-	appConfig config.AppConfig
+type messageRepo struct {
+	appConfig config.App
 }
 
-
-func NewArangoMessageRepository(appConfig config.AppConfig) message.MessageRepository{
+func NewArangoMessageRepository(appConfig config.App) message.MessageRepository {
 	return &messageRepo{
-		appConfig
+		appConfig,
 	}
 }
-
 
 func (a *messageRepo) DirectMessagesList(userID, receiverID, channelID int64) ([]*message.Message, error) {
 	panic("implement me")
